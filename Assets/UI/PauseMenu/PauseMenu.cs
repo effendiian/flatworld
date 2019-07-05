1d71848ac1f791e55940b2180e2927550402b7a9
Hakluzak123 <51970187+Hakluzak123@users.noreply.github.com>
2019-07-05 11:06:22 -0400

    UI Update
    Made escape button do stuff.

--- Assets/UI/PauseMenu/PauseMenu.cs
+++ Assets/UI/PauseMenu/PauseMenu.cs
@@ -5,8 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    public GameObject bGPause; //Background pause menu variable
+
     public void QuitGame()
     {
         Debug.Log("The game would have returned to the main menu");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (bGPause.activeSelf == true)
+                bGPause.SetActive(false);
+            else
+                bGPause.SetActive(true);
+        }
     }
 }
